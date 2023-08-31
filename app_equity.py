02df675edfbebbe40f532ad1841cd9f3643ac865
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-08-31 11:13:30 +0100

    Update app_equity.py

--- app_equity.py
+++ app_equity.py
@@ -241,7 +241,6 @@ def sub_plots_w(df,df_weighted,categories,time_frames,frameworks):
 
 
 
-
 # Significance Plot
 def Significance_plot(df,brands,frameworks):
     st.subheader("Equity Plot w/ Significance (90% confidence interval)")
@@ -357,13 +356,13 @@ def main():
         filepath_mmm,year_mmm,month_mmm,day_mmm,hour_mmm,minute_mmm,second_mmm = mmm_info(data,slang)
 
         if res_weighted == "Yes":
-            st.write(f"* **Equity file version** {market} : {day_equity}/{month_equity}/{year_equity} - {hour_equity}: {minute_equity}: {second_equity}*")
-            st.write(f"*  **Age weighted equity file version** {market_weighted}: {day_equity_w}/{month_equity_w}/{year_equity_w} - {hour_equity_w}: {minute_equity_w}: {second_equity_w}*")
+            st.write(f" **Equity file version** {market} : {day_equity}/{month_equity}/{year_equity} - {hour_equity}: {minute_equity}: {second_equity}")
+            st.write(f"  **Age weighted equity file version** {market_weighted}: {day_equity_w}/{month_equity_w}/{year_equity_w} - {hour_equity_w}: {minute_equity_w}: {second_equity_w}")
 
-            st.write(f"*  **MMM data version** {market} : {day_mmm}/{month_mmm}/{year_mmm} - {hour_mmm}: {minute_mmm}: {second_mmm}*")
+            st.write(f"  **MMM data version** {market} : {day_mmm}/{month_mmm}/{year_mmm} - {hour_mmm}: {minute_mmm}: {second_mmm}")
         else:
-            st.write(f"* **Equity file version** {market}: {day_equity}/{month_equity}/{year_equity} - {hour_equity}: {minute_equity}: {second_equity}*")
-            st.write(f"* **MMM data version** {market} : {day_mmm}/{month_mmm}/{year_mmm}- {hour_mmm} - {minute_mmm}: {second_mmm}*")
+            st.write(f" **Equity file version** {market}: {day_equity}/{month_equity}/{year_equity} - {hour_equity}: {minute_equity}: {second_equity}*")
+            st.write(f" **MMM data version** {market} : {day_mmm}/{month_mmm}/{year_mmm}- {hour_mmm} - {minute_mmm}: {second_mmm}*")
 
 
         # reading the equity file
