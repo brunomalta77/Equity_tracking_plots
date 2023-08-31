0733973b900fa3ce1b220c59c05853956fe63f0e
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-08-31 11:10:42 +0100

    Update app_equity.py

--- app_equity.py
+++ app_equity.py
@@ -357,13 +357,13 @@ def main():
         filepath_mmm,year_mmm,month_mmm,day_mmm,hour_mmm,minute_mmm,second_mmm = mmm_info(data,slang)
 
         if res_weighted == "Yes":
-            st.write(f"*From the {market} market - Equity date {day_equity}/{month_equity}/{year_equity}- Hours: {hour_equity}: {minute_equity}: {second_equity}*")
-            st.write(f"*From the {market_weighted} market - Equity date {day_equity_w}/{month_equity_w}/{year_equity_w}- Hours: {hour_equity_w}: {minute_equity_w}: {second_equity_w}*")
+            st.write(f"* **Equity file version** {market} :{day_equity}/{month_equity}/{year_equity} - {hour_equity}: {minute_equity}: {second_equity}*")
+            st.write(f"*  **Age weighted equity file version** {market_weighted}: {day_equity_w}/{month_equity_w}/{year_equity_w} - {hour_equity_w}: {minute_equity_w}: {second_equity_w}*")
 
-            st.write(f"*From the {market} market - MMM date {day_mmm}/{month_mmm}/{year_mmm}- Hours: {hour_mmm}: {minute_mmm}: {second_mmm}*")
+            st.write(f"*  **MMM data version** {market} :{day_mmm}/{month_mmm}/{year_mmm} - {hour_mmm}: {minute_mmm}: {second_mmm}*")
         else:
-            st.write(f"*From the {market} market - Equity date {day_equity}/{month_equity}/{year_equity}- Hours: {hour_equity}: {minute_equity}: {second_equity}*")
-            st.write(f"*From the {market} market - MMM date {day_mmm}/{month_mmm}/{year_mmm}- Hours: {hour_mmm}: {minute_mmm}: {second_mmm}*")
+            st.write(f"* **Equity file version** {market}: {day_equity}/{month_equity}/{year_equity} - {hour_equity}: {minute_equity}: {second_equity}*")
+            st.write(f"* **MMM data version** {market} : {day_mmm}/{month_mmm}/{year_mmm}- {hour_mmm} - {minute_mmm}: {second_mmm}*")
 
 
         # reading the equity file
