db961a1ef7a0f3e02506b01bc65ff92ff1b8b06e
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-09-04 15:07:57 +0100

    Update app_equity.py

--- app_equity.py
+++ app_equity.py
@@ -552,6 +552,8 @@ def sub_plots_w(df,df_weighted,categories,time_frames,frameworks):
          if time_frame == "years":
                   # Extract unique years from the "time" column
                   unique_years = df_filtered['time'].dt.year.unique()
+                  unique_years_w = df_filtered_w['time'].dt.year.unique()
+
                   
                   # Create subplots with separate figures
                   sub_fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.05)
@@ -565,7 +567,7 @@ def sub_plots_w(df,df_weighted,categories,time_frames,frameworks):
                      sub_fig.add_trace(trace, row=2, col=1)
                   
                   sub_fig.update_xaxes(tickvals=[f"{year}-01-01" for year in unique_years], ticktext=unique_years, tickangle=45, row=1, col=1)
-                  sub_fig.update_xaxes(tickvals=[f"{year}-01-01" for year in unique_years], ticktext=unique_years, tickangle=45, row=2, col=1)
+                  sub_fig.update_xaxes(tickvals=[f"{year}-01-01" for year in unique_years_w], ticktext=unique_years_w, tickangle=45, row=2, col=1)
 
                   sub_fig.update_xaxes(title_text="Unweighted Plot",title_font=dict(color="blue"), row=1, col=1)
                   sub_fig.update_xaxes(title_text="Weighted Plol",title_font=dict(color="red"), row=2, col=1)
