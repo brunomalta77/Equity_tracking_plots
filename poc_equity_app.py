cbadbd75ecccc42362d7b377792e60ad89a43d8b
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2024-07-31 14:47:56 +0100

    Update poc_equity_app.py

--- poc_equity_app.py
+++ poc_equity_app.py
@@ -975,7 +975,7 @@ def main():
                                
                                workspace = join_brand_year[index]
                                year,brand = workspace[0],workspace[1]
-                               st.write(year)
+                               st.write(f"**{year}**")
                                # Assuming you want one column per key in brand_mapping
                                num_columns = len(brand_mapping.keys())
                                # Create the columns
