be845b9ad4d6eff825a03a1444bf83c74754dda0
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2024-09-05 11:23:57 +0100

    Update poc_equity_app.py

--- poc_equity_app.py
+++ poc_equity_app.py
@@ -20,6 +20,7 @@ import io
 import requests
 from metrics import AggregateMetrics
 import math
+from scipy.stats import ttest_ind
 #--------------------------------------------------------------------------------------// Aesthetic Global Variables // -------------------------------------------------------------------------
 user_to_equity = {'Brand Prestige & Love':'AF_Brand_Love','Motivation for Change':'AF_Motivation_for_Change','Consumption Experience':'AF_Consumption_Experience','Supporting Experience':'AF_Supporting_Experience','Value For Money':'AF_Value_for_Money',
    'Total Equity':'Total_Equity',"Awareness":'Framework_Awareness','Saliency':'Framework_Saliency','Affinity':'Framework_Affinity','eSoV':'AA_eSoV', 'Reach':'AA_Reach',
